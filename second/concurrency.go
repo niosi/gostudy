43581e3890a0024fbb67fc4f7c78b656e41b4a75
NICKY <qiyamiky@qq.com>
2018-04-11 11:54:25 +0800

    并发处理gorotine 跟 channel

--- second/concurrency.go
+++ second/concurrency.go
@@ -1,8 +1,36 @@
 package main
 
-import "fmt"
-import "time"
+import (
+	"fmt"
+	"runtime"
+)
 
-func main() {
-	fmt.Print(time.Kitchen)
+func main() { //goroutine
+	runtime.GOMAXPROCS(runtime.NumCPU())
+	ch := make(chan int,10)
+	for i := 0; i <= 10; i++ {
+		go sum(ch, i)
+	}
+	for i := 0; i <= 10; i++ {
+		<-ch
+	}
+	/*go Go(ch)
+	for v := range ch{
+		fmt.Print(v)
+	}*/
+}
+
+func Go(a chan int) {
+	fmt.Println("run start")
+	a <- 0
+	close(a)
+}
+
+func sum(a chan int, index int) {
+	sum := 0
+	for i := 0; i <= 1000000; i++ {
+		sum += i
+	}
+	fmt.Println(index, sum)
+	a <- 0
 }
